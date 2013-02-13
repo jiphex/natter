d5f7cdcf64ccd132605164f3b7a4e311c3e08eea
James Hannah <james@tlyk.eu>
2013-02-13 22:48:20 +0000

    array sub on lib/config

--- lib/config.rb
+++ lib/config.rb
@@ -4,6 +4,10 @@ module Comments
   class Config
     attr_reader :config
   
+    def [](key)
+      @config[key]
+    end
+  
     def initialize(configfile='config.yml')
       @config = YAML.load_file(configfile)
     end
